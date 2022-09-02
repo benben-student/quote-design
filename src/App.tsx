5ffa1274d176dfc5aa1c1c4a9d004b7dc69edee3
benben-student <a441832620@126.com>
2022-09-02 17:38:41 +0800

    feat:complete Menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Button, { ButtonSize, ButtonType } from './components/Button/button.tsx';
-
-
+import MenuItem from './components/Menu/menuItem.tsx';
+import Menu from './components/Menu/menu.tsx';
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} onSelect={(index) => { alert(index) }}>
+          <MenuItem index={0}>cool link</MenuItem>
+          <MenuItem index={1} disabled>cool link 2</MenuItem>
+          <MenuItem index={2}>cool link 3</MenuItem>
+        </Menu>
         <Button className="custom">Hello</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
         <Button disabled={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
